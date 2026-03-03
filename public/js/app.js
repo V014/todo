01ceb55cbd549f4c0ee585231c90f5a8da884b6a
Wanga Kanjala <wangakanjala@gmail.com>
2026-03-03 23:26:32 +0200

    pulling from a db

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,22 @@ function googleLogin() {
     firebase.auth().signInWithPopup(provider)
         .then(result => {
             const user = result.user;
-            document.getElementById('user-name').value = 'Hello ' + user.displayName;
+            document.getElementById('user-name').innerText = 'Hello ' + user.displayName;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+function readDB() {
+    const db = firebase.firestore();
+    const mypost = db.collection('mypost').doc('firstpost');
+
+    mypost.get()
+        .then(doc => {
+            
+            const data = doc.data();
+            document.getElementById('user-data').innerText = data.title + ': ' + data.createdAt
         })
         .catch(error => {
             console.error(error);
