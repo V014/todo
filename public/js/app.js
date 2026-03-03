72b072c09f934f29a98579891fac508899275476
Wanga Kanjala <wangakanjala@gmail.com>
2026-03-03 22:42:29 +0200

    updated api location

--- public/js/app.js
+++ public/js/app.js
@@ -4,4 +4,12 @@ document.addEventListener('DOMContentLoaded', event => {
 
 function googleLogin() {
     const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+        .then(result => {
+            const user = result.user;
+            document.getElementById('user-name').value = 'Hello ' + user.displayName;
+        })
+        .catch(error => {
+            console.error(error);
+        });
 }
